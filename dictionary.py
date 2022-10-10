b60b20672da6a9ff0e87110c0992523e25b49e4d
cmurphey8 <53098445+cmurphey8@users.noreply.github.com>
2022-10-10 10:48:31 -0400

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,22 @@
+# Words in dictionary
+words = []
+
+
+def check(word):
+    """Return true if word is in dictionary else false"""
+    #TODO
+
+def load(dictionary):
+    """Load dictionary into memory, returning true if successful else false"""
+    file = open(dictionary, "r")
+    for line in file:
+        word = line.rstrip()
+        #TODO: add word to dictionary words
+
+    file.close()
+    return True
+
+
+def size():
+    """Returns number of words in dictionary if loaded else 0 if not yet loaded"""
+    #TODO
